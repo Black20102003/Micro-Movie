8ebfea5929c6f02e41fea0695b246ea7e5061345
Black20102003 <thu846528@gmail.com>
2026-02-24 11:03:25 +0630

    Fix import case for Image component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Login } from './components/Login';
 import { Home } from './components/Home';
 import { Search } from './components/Search';
 import { BrowserRouter as Router,Routes,Route,Navigate } from 'react-router-dom';
-import { Image } from './components/image';
+import { Image } from './components/Image';
 import { AuthCallBack } from './components/Session';
 import { MovieDetail } from './components/MovieDetail';
 
